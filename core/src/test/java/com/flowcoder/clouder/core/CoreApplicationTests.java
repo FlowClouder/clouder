43e36a4327a98c964ce1171e789645b05bd935d5
kingkiller <864970002@qq.com>
2022-05-01 21:51:56 +0800

    初始化项目

---
+++ core/src/test/java/com/flowcoder/clouder/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowcoder.clouder.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
